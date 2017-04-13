8d056f67165818b994c8d00b8fa6688982da2762
Seung ho Jung <jungseungho@gmail.com>
2017-04-13 19:38:54 -0400

    playability confirmed on OSX

--- Memories/Assets/Scripts/LoadSceneDreamGame.cs
+++ Memories/Assets/Scripts/LoadSceneDreamGame.cs
@@ -11,15 +11,11 @@ public class LoadSceneDreamGame : MonoBehaviour {
     public int countdown = 0;
 
 	void Update () {
-<<<<<<< HEAD
+
 //		if (countdown == 3) {
-		if (countdown == 1) {
-			SceneManager.LoadScene (levelToLoad);
-=======
 		if (countdown >= 1) {
             Initiate.Fade(levelToLoad, loadToColor, 3.0f);
 //            SceneManager.LoadScene (levelToLoad);
->>>>>>> v4.5
 		}
 	}
 }
